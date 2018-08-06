706c2a3dd46978ee280feaeaa931a1321a7d0dad
Rafał Lipieta <cwgflojdid@wp.pl>
2018-08-06 18:35:54 +0200

    Update WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -2,73 +2,68 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
 
+const environment = env = process.env.NODE_ENV || 'development'
 
 const plugins = [new HtmlWebpackPlugin({
     template: 'client/index.html',
     filename: 'index.html',
     inject: 'body'
-})];
+}),
+new OptimizeJsPlugin({
+    sourceMap: false
+})
+];
+
+if (env === 'production') {
+    plugins.push(
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    )
+}
 
 //webpack.config.js
-module.exports = (env) => {
-    const environment = env || 'production';
-    if (env === 'production') {
-        plugins.push(
-            new OptimizeJsPlugin({
-                sourceMap: false
-            })
-        )
-    }
+module.exports = {
 
-    return {
-        mode: environment,
-        entry: './client/index.js',
-        output: {
-            path: path.resolve(__dirname, 'public'),
-            filename: 'app.bundle.js'
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: 'src/index.html',
-                filename: 'index.html',
-                inject: 'body'
-            }),
-            new OptimizeJsPlugin({
-                sourceMap: false
-            })
+    mode: environment,
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.bundle.js'
+    },
 
-        ],
-        devServer: {
-            proxy: {
-                '/socket.io': {
-                    target: 'http://localhost:3000',
-                    ws: true
-                }
+    devServer: {
+        proxy: {
+            '/socket.io': {
+                target: 'http://localhost:3000',
+                ws: true
             }
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    loader: "babel-loader",
-                    options: {
-                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        { loader: 'style-loader' },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true
-                            }
-                        }
-                    ]
-                }
-            ]
         }
     },
-        { plugins };
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                options: {
+                    plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+
+    },
+    plugins: plugins
 };
